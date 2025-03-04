1d1a1b84284d4484168775401f436f9b0e29cc67
Mehmet Faik <132834431+mehmettfaik@users.noreply.github.com>
2025-03-04 18:04:26 +0300

    Update config.js

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,2 +1,2 @@
 export const BASE_URL = 'https://www.nosyapi.com/apiv2/service/';
-export const API_KEY = 'Td00EAvbEHv7NGImd23gf73HC7wbGzhlcSm816RywIy7zKANa6Okzrhvfyqv';
+export const API_KEY = '**';
